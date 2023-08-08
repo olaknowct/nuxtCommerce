51059c5fd25b92c9936b8ffad1e5666be1d8ba1b
Christopher Olano <christopher.olano@skig.tech>
2023-08-08 17:44:57 +0800

    use layouts directory

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
 <template>
-  <ul>
-    <li>
-      <NuxtLink to="/">Home</NuxtLink>
-    </li>
-    <li>
-      <NuxtLink to="/products">products</NuxtLink>
-    </li>
-  </ul>
-  <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ components/navigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <nav>
+      <ul class="flex flex-row text-xl">
+        <li class="">
+          <NuxtLink class="rounded-xl bg-red-300" to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/products">products</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>

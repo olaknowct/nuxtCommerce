312e6cdd9f7f42ccc9c6e0b1f421b365534c6dd6
Christopher Olano <christopher.olano@skig.tech>
2023-08-09 17:43:26 +0800

    clean up refactor products

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -5,19 +5,13 @@ import { useFetch } from '@vueuse/core';
 const router = useRouter();
 const route = useRoute();
 const store = useProductStore();
-
-console.log(route.query.category);
-
-if (route.query?.category !== 'all') {
-  store.setSelectedCategory(route.query.category);
-}
-// let url;
 const getProdutsEndpoint = 'https://fakestoreapi.com/products';
 let fetchUrl = getProdutsEndpoint;
 
-if (store.selectedCategory !== 'all') {
+if (route.query.category) store.setSelectedCategory(route.query.category);
+
+if (store.selectedCategory !== 'all')
   fetchUrl = `https://fakestoreapi.com/products/category/${store.selectedCategory}`;
-}
 
 const { isFetching, error, data, isFinished } = useFetch(fetchUrl, {
   afterFetch(ctx) {
@@ -42,28 +36,6 @@ watch(
     router.push(`/products?category=${newCategory}`);
   }
 );
-
-// watch(
-//   () => store.selectedCategory,
-//   (newCategory) => {
-//     url =
-//       newCategory == 'all'
-//         ? getProdutsEndpoint
-//         : `https://fakestoreapi.com/products/category/${newCategory}`;
-//     console.log(url);
-//     const { isFetching, error, data, isFinished } = useFetch(url, {
-//       afterFetch(ctx) {
-//         store.setItems(JSON.parse(ctx.data));
-//       },
-//     });
-//     console.log(url, newCategory);
-//   }
-// );
-// // fetching related
-// let url = 'https://fakestoreapi.com/products';
-
-// if (products.selectedCategory != 'all')
-//   url = `https://fakestoreapi.com/products/category/${products.selectedCategory}`;
 </script>
 <template>
   <div>

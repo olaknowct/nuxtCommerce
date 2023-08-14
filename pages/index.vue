91523bbc4c986eca53d9e062908c9ee516849a29
Christopher Olano <christopher.olano@skig.tech>
2023-08-14 18:06:45 +0800

    Refactor/Optimize: use computed instead watch | listening to sort by

--- pages/index.vue
+++ pages/index.vue
@@ -43,26 +43,30 @@ watch(
   }
 );
 
-watch(
-  () => store.sortBy,
-  (newSortBy) => {
-    const items = [...products.items];
+computed(() => {
+  const items = [...products.value.items];
+  let sortedItems;
 
-    if (store.sortBy === 'alphabetically') {
-      store.setItems(items.sort((a, b) => a.title.localeCompare(b.title)));
-    } else if (store.sortBy === 'pricing') {
-      store.setItems(items.sort((a, b) => a.price - b.price));
-    } else if (store.sortBy === 'rating') {
-      store.setItems(items.sort((a, b) => b.rating.rate - a.rating.rate));
-    } else {
-      return items;
-    }
-
-    if (!route.query.category) return router.push(`/products?sortBy=${newSortBy}`);
-    if (!route.query.sortBy) return router.push(`${route.fullPath}&sortBy=${newSortBy}`);
-    router.push(`${route.path}?category=${route.query.category}&sortBy=${newSortBy}`);
+  switch (products.value.sortBy) {
+    case 'alphabetically':
+      sortedItems = items.sort((a, b) => a.title.localeCompare(b.title));
+      break;
+    case 'pricing':
+      sortedItems = items.sort((a, b) => a.price - b.price);
+      break;
+    case 'rating':
+      sortedItems = items.sort((a, b) => b.rating.rate - a.rating.rate);
+      break;
+    default:
+    // code block
   }
-);
+
+  store.setItems(sortedItems);
+
+  if (!route.query.category) return router.push(`/products?sortBy=${products.value.sortBy}`);
+  if (!route.query.sortBy) return router.push(`${route.fullPath}&sortBy=${products.value.sortBy}`);
+  router.push(`${route.path}?category=${route.query.category}&sortBy=${products.value.sortBy}`);
+});
 </script>
 <template>
   <div class="flex flex-col md:flex-row max-w-6xl mx-auto xl:p-0 px-24 gap-10">
